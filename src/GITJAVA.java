fd0026f129aaeae11f1c50a27155e943b443968f
PREETIPP <nammayoho@gmail.com>
2022-06-09 00:35:16 +0530

    First Commit

---
+++ src/GITJAVA.java
@@ -0,0 +1,18 @@
+
+public class GITJAVA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i= 5;
+if (i ==5)
+{
+	System.out.println("I am successful");
+}
+else
+	
+
+{
+	System.out.println("Done");
+	}}}
+
+
